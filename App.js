f511efadc5cfe48d109aa16570f151d715ed0dd9
Gabriel-smile <144184601+Gabriel-smile@users.noreply.github.com>
2026-04-11 02:20:55 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,29 +1,64 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { Ionicons } from '@expo/vector-icons';
 
 import HomeScreen from './src/screens/HomeScreen';
-import BiometriaScreen from './src/screens/BiometriaScreen';
 import CameraScreen from './src/screens/CameraScreen';
-import MapScreen from './src/screens/MapScreen';
-import JurosScreen from './src/screens/JurosScreen';
-import TodoScreen from './src/screens/TodoScreen';
+import PerfilScreen from './src/screens/PerfilScreen';
 import LoginScreen from './src/screens/LoginScreen';
+import BiometriaScreen from './src/screens/BiometriaScreen';
+import JurosScreen from './src/screens/JurosScreen';
+import MapScreen from './src/screens/MapScreen';
+import TodoScreen from './src/screens/TodoScreen';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function Tabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarShowLabel: false, // 🔥 tira texto
+
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'HomeTab') {
+            iconName = focused ? 'home' : 'home-outline';
+          } 
+          else if (route.name === 'CameraTab') {
+            iconName = focused ? 'camera' : 'camera-outline';
+          } 
+          else if (route.name === 'PerfilTab') {
+            iconName = focused ? 'person' : 'person-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen name="HomeTab" component={HomeScreen} />
+
+      <Tab.Screen name="CameraTab" component={CameraScreen} />
+
+      <Tab.Screen name="PerfilTab" component={PerfilScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-    
         <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={Tabs} options={{ headerShown: false }} />
         <Stack.Screen name="Biometria" component={BiometriaScreen} />
-        <Stack.Screen name="Camera" component={CameraScreen} />
-        <Stack.Screen name="Mapa" component={MapScreen} />
         <Stack.Screen name="Juros" component={JurosScreen} />
-        <Stack.Screen name="ToDo" component={TodoScreen} />
-
+        <Stack.Screen name="Mapa" component={MapScreen} />
+        <Stack.Screen name="Todo" component={TodoScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
